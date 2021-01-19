885e47b61fb08e3b5d15e992e820978f7a384efb
Shivam Pandey <shivam.pandey@postman.com>
2021-01-19 19:54:08 +0530

    added more intents

--- index.js
+++ index.js
@@ -30,35 +30,50 @@ app.post('/',express.json(), async (req,res) => {
             response: res
         });
      function feedback(agent){
-        agent.add("we value your feedback, Please give your valuable feedback");
+        // agent.add("we value your feedback, Please give your valuable feedback");
+        agent.add( new dfff.Card({
+            title: `Title: this is a card title`,
+            imageUrl: "https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg",
+            text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
+            buttonText: 'This is a button',
+            buttonUrl: "https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg"
+          }));
+
     }
 
     function rating(agent){
         agent.add("please give your rating for the most recent purchase");
     }
 
-    async function order(agent){
-        let payloadData = {
-            "richContent": [
-              [
-                {
-                  "type": "Food",
-                  "title": "Mix Veg",
-                  "subtitle": "Mix Veg curry",
-                  "image": {
-                    "src": {
-                      "rawUrl": "https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg"
-                    }
-                  },
-                  "text": "Order in progress"
-                }
-              ]
-            ]
-          }
-            await agent.add( new dfff.Payload(agent.UNSPECIFIED, payloadData, {rawPayload: true, sendAsMessage: true}));
-        };
+    function order(agent){
+        // let payloadData = {
+        //     "richContent": [
+        //       [
+        //         {
+        //           "type": "Food",
+        //           "title": "Mix Veg",
+        //           "subtitle": "Mix Veg curry",
+        //           "image": {
+        //             "src": {
+        //               "rawUrl": "https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg"
+        //             }
+        //           }, 
+        //           "text": "Order in progress"
+        //         }
+        //       ]
+        //     ]
+        //   }
+            agent.add( new dfff.Card({
+                title: `Title: this is a card title`,
+                imageUrl: "https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg",
+                text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
+                buttonText: 'This is a button',
+                buttonUrl: "https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg"
+              }));
+        }
 
     async function userDetails(agent){
+        console.log('agent-->',agent);
         try{
             let userPayload = {
                 "richContent": [
@@ -80,13 +95,13 @@ app.post('/',express.json(), async (req,res) => {
                 console.log("err-->",err);
             }
         }
-    };
+    }
 
     let intentMap = new Map();
 
     intentMap.set('feedback', feedback);
     intentMap.set('rating', rating);
-    intentMap.set('order', order);
+    intentMap.set('myorder', order);
     intentMap.set('user', userDetails);
 
     //handling all the request for all the respective intents
